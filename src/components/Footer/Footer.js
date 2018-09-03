b3846b90af8b18eb6c99b4663c3a5e342160ab75
Brevon Good <brevonbgood@comcast.net>
2018-09-02 23:24:06 -0600

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className='footer wrapper'>
-        <p className='legal'>2018 Brevon Good. This site is heavily based off Seb Kay's personal site found <a className='seb-kay' href='https://sebkay.com/'>here.</a>
+        <p className='legal'>2018 Brevon Good.
         <br/>
         Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></p>
 
